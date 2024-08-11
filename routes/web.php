4256dabe658116549d6b0d1e8dc8f578c6a2004e
Ree <rAlayed48@gmail.com>
2024-08-11 16:31:12 +0300

    adding new pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\content;
 
-Route::get('/', function () { return view('index');
+//---------------- shows بانوراما  page
+Route::get('/', function () {
+    return view('home');
+});
+
+//---------------- shows create form page
+Route::get('/create', function () {
+    return view('create');
+});
+
+//---------------- shows من نحن page
+Route::get('/about', function () {
+    return view('about');
+});
+
+//---------------- returns خدماتنا page with any contents that has the type 0
+Route::get('/services', function () {
+    return view('services', [
+        'contents' => content::all()->where('type', 0)
+
+    ]);
+});
+
+//---------------- returns عملائنا page with any contents that has the type 1
+Route::get('/works', function () {
+    return view('works', [
+        'contents' => content::all()->where('type', 1)
+    ]);
+});
+
+//---------------- shows تواصل معنا page
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
+
+//---------------- shows بانوراما القديمه
+Route::get('/index', function () {
+    return view('index');
 });
