d8fcb0fee67af96cc8e623c25f0f3c67ceaaba96
Ree <rAlayed48@gmail.com>
2024-08-13 10:31:55 +0300

    updating controller

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -1,10 +1,85 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\content;
 use Illuminate\Http\Request;
 
 class ContentController extends Controller
 {
-    //
+    // '/create'
+    public function create(){
+        request()->validate([
+            'Title' => ['required'],
+            'Photo' => ['required'],
+            'type' => ['required']
+        ]);
+        $photo = 'photo' . uniqid() . '.' . request('Photo')->extension();
+        request('Photo')->move(public_path('images'), $photo);
+        content::create([
+            'Title' => request('Title'),
+            'Photo' => "images/$photo", //request('Photo'),
+            'description' => request('description'),
+            'type' => request('type')
+        ]);
+        if (request('type') == 0){
+            return redirect('/services');
+        } else {
+            return redirect('/works');
+        };
+    }
+
+    // '/services'
+    public function service(){
+        return view('services', [
+            'contents' => content::all()->where('type', 0)
+        ]);
+    }
+
+    // '/works'
+    public function work(){
+        return view('works', [
+            'contents' => content::all()->where('type', 1)
+        ]);
+    }
+
+    // '/edit/{content}'
+    public function edit(content $content){
+        return view('edit', ['content' => $content]);
+    }
+    
+    // '/edit/{content}'
+    public function update(content $content){
+    //authorize
+
+    request()->validate([
+        'Title' => ['required'],
+        'Photo' => ['required'],
+        'type' => ['required']
+    ]);
+    $photo = 'photo' . uniqid() . '.' . request('Photo')->extension();
+    request('Photo')->move(public_path('images'), $photo);
+    $content->update([
+        'Title' => request('Title'),
+        'Photo' => "images/$photo", //request('Photo'),
+        'description' => request('description'),
+        'type' => request('type')
+    ]);
+    if ($content['type']==0) {
+        return redirect('/services');
+    } else {
+        return redirect('/works');
+    };
+    }
+
+    public function destroy(content $content){
+    //authorize
+
+    $content->delete();
+    if ($content['type']==0) {
+        return redirect('/services');
+    } else {
+        return redirect('/works');
+    };
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,125 +1,36 @@
 <?php
 
+use App\Http\Controllers\ContentController;
 use Illuminate\Support\Facades\Route;
-use App\Models\content;
 
 //---------------- shows بانوراما  page
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/','home');
 
 //---------------- shows create form page
-Route::get('/create', function () {
-    return view('create');
-});
+Route::view('/create', 'create');
 
-//---------------- stores content
-Route::post('/create', function () {
-    //dd(request()->all());
-    request()->validate([
-        'Title' => ['required'],
-        'Photo' => ['required'],
-        'type' => ['required']
-    ]);
+//---------------- controller
+Route::controller(ContentController::class)->group( function () {
+    //---------------- stores content
+    Route::post('/create', 'create');
+    //---------------- returns خدماتنا page with any contents that has the type 0
+    Route::get('/services', 'service');
+    //---------------- returns عملائنا page with any contents that has the type 1
+    Route::get('/works', 'work');
+    //---------------- shows edit form page
+    Route::get('/edit/{content}', 'edit');
+    //---------------- update
+    Route::patch('/edit/{content}', 'update');
+    //---------------- destroy
+    Route::delete('/edit/{content}', 'destroy');
 
-    $photo = 'photo' . uniqid() . '.' . request('Photo')->extension();
-    request('Photo')->move(public_path('images'), $photo);
-
-    content::create([
-        'Title' => request('Title'),
-        'Photo' => "images/$photo", //request('Photo'),
-        'description' => request('description'),
-        'type' => request('type')
-    ]);
-
-    return redirect('/');
 });
 
 //---------------- shows من نحن page
-Route::get('/about', function () {
-    return view('about');
-});
-
-//---------------- returns خدماتنا page with any contents that has the type 0
-Route::get('/services', function () {
-    return view('services', [
-        'contents' => content::all()->where('type', 0)
-
-    ]);
-});
-
-//---------------- returns عملائنا page with any contents that has the type 1
-Route::get('/works', function () {
-    return view('works', [
-        'contents' => content::all()->where('type', 1)
-    ]);
-});
-
-//---------------- shows edit form page
-Route::get('/edit/{id}', function ($id) {
-    $content = content::find($id);
-    return view('edit', ['content' => $content]);
-
-    //return view('');
-});
-
-//---------------- update
-Route::patch('/edit/{id}', function ($id) {
-    request()->validate([
-        'Title' => ['required'],
-        'Photo' => ['required'],
-        'type' => ['required']
-    ]);
-    //authorize
-    $content = content::findOrFail($id);
-
-    $photo = 'photo' . uniqid() . '.' . request('Photo')->extension();
-    request('Photo')->move(public_path('images'), $photo);
-    $content->update([
-        'Title' => request('Title'),
-        'Photo' => "images/$photo", //request('Photo'),
-        'description' => request('description'),
-        'type' => request('type')
-    ]);
-    if ($content['type']==0) {
-        return redirect('/services');
-    } else {
-        return redirect('/works');
-    };
-
-    // $content = content::find($id);
-    // if ('type'== 0)
-    //     $path = "services" ;
-    // if ('type'== 1)
-    // $path = "works" ;
-    // return view("/$path", ['content'=> $content]);
-});
-
-//---------------- delete
-Route::delete('/edit/{id}', function ($id) {
-    //authorize
-    $content = content::findOrFail($id);
-    $content->delete();
-    if ($content['type']==0) {
-        return redirect('/services');
-    } else {
-        return redirect('/works');
-    };
-
-    // $content = content::find($id);
-    // if ('type'== 0)
-    //     $path = "services" ;
-    // if ('type'== 1)
-    // $path = "works" ;
-    // return view("/$path", ['content'=> $content]);
-});
+Route::view('/about', 'about');
 
 //---------------- shows تواصل معنا page
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-
+Route::view('/contact', 'contact');
 
 
 
